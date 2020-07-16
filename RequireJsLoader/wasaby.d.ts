843cc659aec66b8929e7c6cf9904795efa4b4379
aa.malcev2 <aa.malcev2@tensor.ru>
2020-07-16 19:20:40 +0300

    Turn config.js to config.ts https://online.sbis.ru/opendoc.html?guid=62fa03af-7aaf-4818-9a31-50b0f1e8040c

--- RequireJsLoader/wasaby.d.ts
+++ RequireJsLoader/wasaby.d.ts
@@ -1,17 +1,55 @@
+import { IRequireExt } from './require.ext';
+
+export type BuildMode = 'release' | 'debug';
+
+export interface IPatchedGlobal {
+    bundles: string[];
+    console: typeof console;
+    contents: IContents;
+    define: RequireDefine;
+    require: Require;
+    requirejs: IRequireExt;
+    wsConfig: IWsConfig;
+    rtpackModuleNames: string;
+}
+
+export interface IPatchedWindow extends Window, IPatchedGlobal {
+}
+
+export interface IStaticResourcesConfig {
+    domains: string[];
+    resources?: string[];
+    types: string[];
+ }
+
+
 export interface IWsConfig {
+    APP_PATH?: string;
     BUILD_MODE?: string;
     DEBUGGING_MODULES?: string[];
     IS_OVERALL_DEBUG?: boolean;
     IS_SERVER_SCRIPT?: boolean;
+    RESOURCES_PATH?: string;
+    appRoot?: string;
+    debug?: boolean;
+    product?: string;
+    resourceRoot?: string;
+    staticDomains?: IStaticResourcesConfig;
     showAlertOnTimeoutInBrowser?: boolean;
+    wsRoot?: string;
+    versioning?: boolean;
 }
 
 export interface IContents {
-    buildnumber: string;
+    availableLanguage?: object;
+    bundles?: object;
+    buildMode?: BuildMode;
+    buildnumber?: string;
+    contextVersion?: string;
+    loadedServices?: Record<string, boolean>;
     modules: {
        [key: string]: IModule;
     };
-    availableLanguage?: object;
 }
 
 interface IModule {
