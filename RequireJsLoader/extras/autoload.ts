cb13d9396e71a366ed451a06bdad733d89bb9e0e
aa.malcev2 <aa.malcev2@tensor.ru>
2020-09-08 10:34:24 +0300

    Undefine failed modules depending on registry state. https://online.sbis.ru/opendoc.html?guid=0e946bc3-483a-48d9-91d3-ade8c162ce56

--- RequireJsLoader/extras/autoload.ts
+++ RequireJsLoader/extras/autoload.ts
@@ -1,4 +1,4 @@
-import errorHandler from './errorHandler';
+import errorHandler, {ILogger} from './errorHandler';
 import resourceLoadHandler from './resourceLoadHandler';
 import patchDefine from './patchDefine';
 import {getInstance} from './utils';
@@ -8,18 +8,36 @@ let restoreErrorHandler;
 let restoreResourceLoadCallback;
 let restoreDefine;
 
+interface IIoC {
+    resolve<T>(name: string): T;
+}
+
+// Module which supplies logger
+const logSupplierModule = 'Env/Env';
+const defaultLogger = console;
+
+const logger: ILogger = {
+    log(tag: string, message: string): void {
+        import(logSupplierModule).then(({IoC}: {IoC: IIoC}) => {
+            IoC.resolve<ILogger>('ILogger').log(tag, message);
+        }).catch(() => {
+            defaultLogger.log(tag, message);
+        });
+    }
+};
+
 /**
  * Provides a few patches for RequireJS through official and semi-official API
  * @author Мальцев А.А.
  */
-function autoload(force?: boolean): () => void {
+function autoload(): () => void {
     if (!patchApplied) {
         patchApplied = true;
 
         const require = getInstance();
         if (require) {
-            restoreErrorHandler = errorHandler(require, force);
-            restoreResourceLoadCallback = resourceLoadHandler(require, force);
+            restoreErrorHandler = errorHandler(require, {logger});
+            restoreResourceLoadCallback = resourceLoadHandler(require);
         }
 
         restoreDefine = patchDefine();
