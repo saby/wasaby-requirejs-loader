8d9e9081a1da87e1fe9016dc9bf5cf87bdce9b8b
aa.malcev2 <aa.malcev2@tensor.ru>
2020-10-21 17:02:19 +0300

    Don't cut URL on client side APP_PATH. https://online.sbis.ru/opendoc.html?guid=2a17206d-8c2b-467b-beb1-a7a1d4ac6366

--- RequireJsLoaderUnit/_conduct/getModuleUrl.test.ts
+++ RequireJsLoaderUnit/_conduct/getModuleUrl.test.ts
@@ -36,4 +36,16 @@ describe('RequireJsLoader/_conduct/getModuleUrl', () => {
         wsConfig.staticDomains = {domains: ['foo.bar']};
         assert.equal(getModuleUrl('RequireJsLoader/conduct'), '//foo.bar/RequireJsLoader/conduct.js');
     });
+
+    it('should return URL with domain name', () => {
+        wsConfig.APP_PATH = '/';
+
+        const context = requirejs.s.contexts._;
+        const originalNameToUrl = context.nameToUrl;
+        context.nameToUrl = () => '//foo.bar/RequireJsLoader/conduct.js';
+        const result = getModuleUrl('RequireJsLoader/conduct');
+        context.nameToUrl = originalNameToUrl;
+
+        assert.equal(result , '//foo.bar/RequireJsLoader/conduct.js');
+    });
 });
