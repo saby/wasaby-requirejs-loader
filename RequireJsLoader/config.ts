c70d7491199b56f9058a433c29e2ce1dc8a19b42
Ivan Mogilevsky <mogilevsky.ivan@gmail.com>
2020-12-21 17:35:36 +0300

    Поменял конкатенацию строк на шаблонные https://online.sbis.ru/opendoc.html?guid=11cd685f-b20f-44df-8221-deefe0871584

--- RequireJsLoader/config.ts
+++ RequireJsLoader/config.ts
@@ -846,8 +846,8 @@ define('RequireJsLoader/config', (() => {
                 tmpl: pathJoin(resourcesPath, 'RequireJsLoader/plugins/tmpl'),
                 wml: pathJoin(resourcesPath, 'RequireJsLoader/plugins/wml'),
                 xml: pathJoin(resourcesPath, 'RequireJsLoader/plugins/xml'),
-                react: REACT_CDN_PATH + (DEBUG_MODE ? 'react.development' : 'react.production.min'),
-                'react-dom': REACT_CDN_PATH + (DEBUG_MODE ? 'react-dom.development' : 'react-dom.production.min'),
+                react: `${REACT_CDN_PATH}${DEBUG_MODE ? 'react.development' : 'react.production.min'}`,
+                'react-dom': `${REACT_CDN_PATH}${DEBUG_MODE ? 'react-dom.development' : 'react-dom.production.min'}`,
                 // jQuery must die
                 jquery: '/cdn/JQuery/jquery/3.3.1/jquery-min'
 
