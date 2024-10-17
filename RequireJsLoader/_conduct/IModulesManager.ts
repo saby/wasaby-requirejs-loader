4d2860518d5d882c945ff4bcd6782bc80dbc747b
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2024-10-17 18:01:54 +0300

    new change

---
+++ RequireJsLoader/_conduct/IModulesManager.ts
@@ -0,0 +1,26 @@
+export type ModulesManagerConstructor = new () => IModulesManager;
+
+/**
+ * Интерфейс менеджера модулей
+ * @private
+ */
+export default interface IModulesManager {
+    /**
+     * Проверяет, что моудль загружен
+     * @param module Имя модуля
+     */
+    isLoaded(module: string): boolean;
+
+    /**
+     * Загружает модули с указанными именами
+     * @param modules Имена модулей для загрузки
+     * @returns Загруженные модули
+     */
+    load<T>(modules: string[]): Promise<T>;
+
+    /**
+     * Выгружает модули с указанными именами
+     * @param modules Имена модулей для загрузки
+     */
+    unload(modules: string[]): Promise<void>;
+}
