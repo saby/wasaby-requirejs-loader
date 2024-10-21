fec6d7f55742fbe3838938504647273b6fe60d47
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2024-10-21 18:02:25 +0300

    new change

---
+++ Jenkinsfile
@@ -0,0 +1,9 @@
+@Library('pipeline') _
+
+def version = '24.5100'
+
+node (get_label()) {
+    checkout_pipeline("rc-${version}")
+    run_branch = load '/home/sbis/jenkins_pipeline/platforma/branch/run_branch'
+    run_branch.execute('wasaby_requirejs_loader', version)
+}
---
+++ RequireJsLoader/_conduct/IModulesManagerSync.ts
@@ -0,0 +1,20 @@
+export type ModulesManagerSyncConstructor = new () => IModulesManagerSync;
+
+/**
+ * Интерфейс синхронного менеджера модулей
+ * @private
+ */
+export default interface IModulesManagerSync {
+    /**
+     * Загружает модуль с указанными именами
+     * @param module Имена модулей для загрузки
+     * @returns Загруженные модули
+     */
+    loadSync<T>(module: string): T;
+
+    /**
+     * Выгружает модули с указанными именами
+     * @param module Имена модулей для выгрузки
+     */
+    unloadSync(module: string): void;
+}
