e890507b653948b58fab8d8fc464ba568ec7eb9b
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2024-10-21 18:02:24 +0300

    new change

---
+++ WasabyLoaderUnit/resources/TestLibraryAsync.js
@@ -0,0 +1,11 @@
+define(['WasabyLoaderUnit/resources/TestControlAsync'], function (TestControlAsync) {
+   return {
+      exportFunction: function (echo) {
+         return echo;
+      },
+      exportSyncFunction: function (echo) {
+         return echo;
+      },
+      TestControlAsync: TestControlAsync
+   };
+});
