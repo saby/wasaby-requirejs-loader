47e9b373837f476f65187dafea8076f51ed792f6
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2025-04-05 18:02:26 +0300

    new change

---
+++ RequireJsLoader/_extras/utils.ts
@@ -0,0 +1,34 @@
+import { IRequireExt } from '../requireTypes';
+import { IPatchedGlobal } from '../wasaby';
+
+/**
+ * Returns global RequireJS instance
+ */
+export function getInstance(): IRequireExt {
+    return (globalThis as unknown as IPatchedGlobal).requirejs;
+}
+
+/**
+ * Returns UI module name from AMD module name
+ * @param inititalAmdModuleName Module name in AMD format
+ * @param removePlugin Remove plugin names from module name
+ */
+export function getInterfaceModuleName(
+    inititalAmdModuleName: string,
+    removePlugin?: boolean
+): string {
+    let amdModuleName = inititalAmdModuleName;
+    if (removePlugin) {
+        const pluginSeparatorPosition = amdModuleName.lastIndexOf('!');
+        if (pluginSeparatorPosition !== -1) {
+            amdModuleName = amdModuleName.substr(1 + pluginSeparatorPosition);
+
+            const pluginParamsPosition = amdModuleName.indexOf('?');
+            if (pluginParamsPosition !== -1) {
+                amdModuleName = amdModuleName.substr(1 + pluginParamsPosition);
+            }
+        }
+    }
+
+    return amdModuleName.split('/')[0];
+}
