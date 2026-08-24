fa7de5427bb8759db5a1e70796007139cf3ef338
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2026-08-24 18:05:29 +0300

    new change

---
+++ RequireJsLoader/_conduct/IModulesHandler.ts
@@ -0,0 +1,22 @@
+/**
+ *
+ */
+export type ModuleLoadCallback = (name: string, implementation: unknown) => unknown;
+
+/**
+ * Интерфейс обработчика модулей
+ * @private
+ */
+export default interface IModulesHandler {
+    /**
+     * Подключает обработчик загрузки модуля
+     * @param callback Обработчик
+     */
+    onModuleLoaded(callback: ModuleLoadCallback): void;
+
+    /**
+     * Отключает обработчик загрузки модуля
+     * @param callback Обработчик
+     */
+    offModuleLoaded(callback: ModuleLoadCallback): void;
+}
