bbe4ef9dbe4a7b2bc270b859e4586fd1662a2fe0
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2026-09-18 18:05:02 +0300

    new change

---
+++ RequireJsLoader/loadFont.ts
@@ -0,0 +1,91 @@
+import { Head as HeadAPI } from 'Application/Page';
+import Env = require('optional!Env/Env');
+import { getModuleUrl } from 'RequireJsLoader/conduct';
+
+const CEF_CONVERTER_BROWSER_NAME = 'SbisCefConverter';
+const OLD_VERSION_CHROME = 80;
+
+const DEFAULT_SIZE = 's';
+const FONT_SIZES = ['s', 'st', 'm'];
+
+function detectSize(size?: string) {
+    if (size && FONT_SIZES.indexOf(size) !== -1) {
+        return size;
+    }
+
+    return DEFAULT_SIZE;
+}
+
+function isCefConverterBrowser(): boolean {
+    return Env.constants.browser.chrome
+        ? Env.constants.browser.userAgent.includes(CEF_CONVERTER_BROWSER_NAME)
+        : false;
+}
+
+/**
+ * Определяет нужно ли грузить шрифты совместимости.
+ */
+export function isCompatibleMode(): boolean {
+    try {
+        return (
+            isCefConverterBrowser() ||
+            (Env.detection.chrome && Env.detection.chromeVersion < OLD_VERSION_CHROME)
+        );
+    } catch (e) {
+        return true;
+    }
+}
+
+function getResourcesPaths(name: string, size: string): string[] {
+    const nameFont = isCompatibleMode() ? `${name}_${size}_compatible` : `${name}_${size}`;
+
+    return [getModuleUrl(`${nameFont}.woff2`), getModuleUrl(`${nameFont}.css`)];
+}
+
+/**
+ * Функция загрузку шрифта.
+ * @param name Имя шрифта. Пример, Controls-icons/actions
+ * @param size Размер шрифта.
+ * @param onLoad Колбек на успешную загрузку.
+ * @param onError Колбек на ошибку.
+ */
+export default function loadFont(
+    name: string,
+    size?: string,
+    onLoad?: () => void,
+    onError?: (err: Error) => void
+): void {
+    const callback = onLoad || (() => {});
+    const [fontHref, cssHref] = getResourcesPaths(name, detectSize(size));
+
+    HeadAPI.getInstance().createTag(
+        'link',
+        {
+            rel: 'preload',
+            type: 'font/woff2',
+            as: 'font',
+            href: fontHref,
+            crossorigin: 'anonymous',
+        },
+        null,
+        {
+            load: () => {
+                HeadAPI.getInstance().createTag(
+                    'link',
+                    {
+                        rel: 'stylesheet',
+                        type: 'text/css',
+                        href: cssHref,
+                        crossorigin: 'anonymous',
+                    },
+                    null,
+                    {
+                        load: callback,
+                        error: onError,
+                    }
+                );
+            },
+            error: onError,
+        }
+    );
+}
